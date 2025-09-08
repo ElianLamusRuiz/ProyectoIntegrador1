24ed2bcd8b56688b47945b165bb999fe742ede09
ElianLamusRuiz <elianclamus10@gmail.com>
2025-09-08 00:14:53 -0500

    Setup Spring Boot

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/AgroTeachNovaApplication.java
@@ -0,0 +1,13 @@
+package com.AgrotechNova.AgroTeachNova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroTeachNovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroTeachNovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AgrotechNova/AgroTeachNova/AgroTeachNovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgrotechNova.AgroTeachNova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroTeachNovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
