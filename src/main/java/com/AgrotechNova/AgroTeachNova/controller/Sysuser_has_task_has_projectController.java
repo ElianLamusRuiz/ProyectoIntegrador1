c26b79035f8a755335eae719d024dca8bcfca981
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:36:02 -0500

    Create Sysuser_has_task_has_projectController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/Sysuser_has_task_has_projectController.java
@@ -0,0 +1,23 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.Sysuser_has_task_has_project;
+import com.AgrotechNova.AgroTeachNova.service.Sysuser_has_task_has_projectService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sysuser_has_task_has_project")
+public class Sysuser_has_task_has_projectController {
+
+    private final Sysuser_has_task_has_projectService sysuser_has_task_has_projectService;
+
+    public Sysuser_has_task_has_projectController(Sysuser_has_task_has_projectService sysuser_has_task_has_projectService) {
+        this.sysuser_has_task_has_projectService = sysuser_has_task_has_projectService;
+    }
+
+    @GetMapping
+    public List<Sysuser_has_task_has_project> getAllSysuser_has_task_has_project() {
+        return sysuser_has_task_has_projectService.getAllSysuser_has_task_has_project();
+    }
+}
