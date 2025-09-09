575ced75d1c880931bd18be5a90730a9896fc0b2
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 21:06:21 -0500

    Create TaskReport.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/TaskReport.java
@@ -0,0 +1,72 @@
+package com.AgrotechNova.AgroTeachNova;
+
+import java.util.Date;
+
+public class TaskReport {
+    private int idtaskReport;
+    private String title;
+    private String description;
+    private Sysuser idsysuser;
+    private Date reportDate;
+    private Task_has_project projectTaskid;
+
+    public TaskReport() {
+    }
+
+    public TaskReport(int idtaskReport, String title, String description, Sysuser idsysuser, Date reportDate, Task_has_project projectTaskid) {
+        this.idtaskReport = idtaskReport;
+        this.title = title;
+        this.description = description;
+        this.idsysuser = idsysuser;
+        this.reportDate = reportDate;
+        this.projectTaskid = projectTaskid;
+    }
+
+    public int getIdtaskReport() {
+        return idtaskReport;
+    }
+
+    public void setIdtaskReport(int idtaskReport) {
+        this.idtaskReport = idtaskReport;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Sysuser getIdsysuser() {
+        return idsysuser;
+    }
+
+    public void setIdsysuser(Sysuser idsysuser) {
+        this.idsysuser = idsysuser;
+    }
+
+    public Date getReportDate() {
+        return reportDate;
+    }
+
+    public void setReportDate(Date reportDate) {
+        this.reportDate = reportDate;
+    }
+
+    public Task_has_project getProjectTaskid() {
+        return projectTaskid;
+    }
+
+    public void setProjectTaskid(Task_has_project projectTaskid) {
+        this.projectTaskid = projectTaskid;
+    }
+}
