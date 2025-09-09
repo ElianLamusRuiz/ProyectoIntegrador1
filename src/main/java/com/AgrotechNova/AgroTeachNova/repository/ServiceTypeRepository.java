a75d71861cb29a328393a0cfe7e47203797bc595
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 22:27:02 -0500

    Create ServiceTypeRepository.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/ServiceTypeRepository.java
@@ -0,0 +1,8 @@
+package com.AgrotechNova.AgroTeachNova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.AgrotechNova.AgroTeachNova.ServiceType;
+
+public interface ServiceTypeRepository extends JpaRepository<ServiceType,Long> {
+
+}
