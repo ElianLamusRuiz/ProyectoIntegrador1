0786352cd1617d1f86bf50de96dd58ac590044e0
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:37:30 -0500

    Create TaskReportController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/TaskReportController.java
@@ -0,0 +1,23 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.TaskReport;
+import com.AgrotechNova.AgroTeachNova.service.TaskReportService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/task_report")
+public class TaskReportController {
+
+    private final TaskReportService taskReportService;
+
+    public TaskReportController(TaskReportService taskReportService) {
+        this.taskReportService = taskReportService;
+    }
+
+    @GetMapping
+    public List<TaskReport> getAllTaskReport() {
+        return taskReportService.getAllTaskReport();
+    }
+}
