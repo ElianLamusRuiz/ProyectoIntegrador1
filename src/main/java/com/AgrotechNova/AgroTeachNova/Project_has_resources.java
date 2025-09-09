a4c2350c4db20fe1b30bb017573a9c6bd2964fbc
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 20:59:33 -0500

    Create Project_has_resources.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/Project_has_resources.java
@@ -0,0 +1,30 @@
+package com.AgrotechNova.AgroTeachNova;
+
+public class Project_has_resources {
+    private Project idproject;
+    private Resources idresources;
+
+    public Project_has_resources() {
+    }
+
+    public Project_has_resources(Project idproject, Resources idresources) {
+        this.idproject = idproject;
+        this.idresources = idresources;
+    }
+
+    public Project getIdproject() {
+        return idproject;
+    }
+
+    public void setIdproject(Project idproject) {
+        this.idproject = idproject;
+    }
+
+    public Resources getIdresources() {
+        return idresources;
+    }
+
+    public void setIdresources(Resources idresources) {
+        this.idresources = idresources;
+    }
+}
