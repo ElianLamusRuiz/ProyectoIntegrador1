d449e62b8b496369c1f3a084e5c476d7d9f2f4d6
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 21:08:56 -0500

    Create Sysuser_has_task_has_project.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/Sysuser_has_task_has_project.java
@@ -0,0 +1,31 @@
+package com.AgrotechNova.AgroTeachNova;
+
+public class Sysuser_has_task_has_project {
+
+    private int sysuser_idsysuser;
+    private int task_has_project_projectTaskid;
+
+    public Sysuser_has_task_has_project() {
+    }
+
+    public Sysuser_has_task_has_project(int sysuser_idsysuser, int task_has_project_projectTaskid) {
+        this.sysuser_idsysuser = sysuser_idsysuser;
+        this.task_has_project_projectTaskid = task_has_project_projectTaskid;
+    }
+
+    public int getSysuser_idsysuser() {
+        return sysuser_idsysuser;
+    }
+
+    public void setSysuser_idsysuser(int sysuser_idsysuser) {
+        this.sysuser_idsysuser = sysuser_idsysuser;
+    }
+
+    public int getTask_has_project_projectTaskid() {
+        return task_has_project_projectTaskid;
+    }
+
+    public void setTask_has_project_projectTaskid(int task_has_project_projectTaskid) {
+        this.task_has_project_projectTaskid = task_has_project_projectTaskid;
+    }
+}
