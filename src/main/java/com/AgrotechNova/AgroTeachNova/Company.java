a2dd37e78002e9b343f7880e33e83db47b217f09
ElianLamusRuiz <148980221+ElianLamusRuiz@users.noreply.github.com>
2025-09-08 22:17:31 -0500

    Update Company.java

--- src/main/java/com/AgrotechNova/AgroTeachNova/Company.java
+++ src/main/java/com/AgrotechNova/AgroTeachNova/Company.java
@@ -11,7 +11,7 @@ public class Company {
 	private String mision;
 	private String vision;
 	
-public Company()
+public Company(){}
 	
 	public Company(int idcompany, String companyName, CompanyType companyType, String description, String mision,
 			String vision) {
