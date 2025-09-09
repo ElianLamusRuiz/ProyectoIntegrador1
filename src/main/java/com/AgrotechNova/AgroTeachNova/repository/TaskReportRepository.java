c0d67a4b4884bbbf6a8d0ca6dbc206fab8a6bb23
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 22:31:36 -0500

    Create TaskReportRepository.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/TaskReportRepository.java
@@ -0,0 +1,8 @@
+package com.AgrotechNova.AgroTeachNova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.AgrotechNova.AgroTeachNova.TaskReport;
+
+public interface TaskReportRepository extends JpaRepository<TaskReport,Long> {
+
+}
