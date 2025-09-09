18dc9f4193f45275f04cd0f69991ab710da909f7
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 22:34:28 -0500

    Create Task_has_projectRepository.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/Task_has_projectRepository.java
@@ -0,0 +1,8 @@
+package com.AgrotechNova.AgroTeachNova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.AgrotechNova.AgroTeachNova.Task_has_project;
+
+public interface Task_has_projectRepository extends JpaRepository<Task_has_project,Long> {
+
+}
