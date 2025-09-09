097d7c6fb9de43df0be04f67c02e67f5acf8f8d3
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 22:30:07 -0500

    Create Sysuser_has_task_has_projectRepository.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/Sysuser_has_task_has_projectRepository.java
@@ -0,0 +1,8 @@
+package com.AgrotechNova.AgroTeachNova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.AgrotechNova.AgroTeachNova.Sysuser_has_task_has_project;
+
+public interface Sysuser_has_task_has_projectRepository extends JpaRepository<Sysuser_has_task_has_project,Long> {
+
+}
