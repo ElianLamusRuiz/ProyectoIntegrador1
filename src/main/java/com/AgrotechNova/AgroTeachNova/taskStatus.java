e09f1940d17aaa713d4c2e85d3d34056299ea03a
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 20:17:21 -0500

    Create taskStatus.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/taskStatus.java
@@ -0,0 +1,28 @@
+
+public class taskStatus {
+  private int idtaskStatus;
+  private String description;
+
+  public taskStatus() {
+  }
+
+  public taskStatus(int idtaskStatus, String description) {
+    this.idtaskStatus = idtaskStatus;
+    this.description = description;
+  }
+
+  public int getIdtaskStatus() {
+    return idtaskStatus;
+  }
+
+  public void setIdtaskStatus(int idtaskStatus) {
+    this.idtaskStatus = idtaskStatus;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
