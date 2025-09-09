a36644c74424f54ce18b4a7e5e6edb1a45f224fc
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 22:35:14 -0500

    Create UserRoleRepository.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/UserRoleRepository.java
@@ -0,0 +1,8 @@
+package com.AgrotechNova.AgroTeachNova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.AgrotechNova.AgroTeachNova.UserRole;
+
+public interface UserRoleRepository extends JpaRepository<UserRole,Long> {
+
+}
