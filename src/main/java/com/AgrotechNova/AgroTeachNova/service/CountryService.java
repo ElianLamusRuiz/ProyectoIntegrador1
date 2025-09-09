429cb394eca68dccb937700ab74bf3c7ed34d322
Daniela080 <142316432+Daniela080@users.noreply.github.com>
2025-09-09 14:03:15 -0500

    Create CountryService.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/service/CountryService.java
@@ -0,0 +1,21 @@
+package com.AgrotechNova.AgroTeachNova.service;
+
+import com.AgrotechNova.AgroTeachNova.Country;
+import com.AgrotechNova.AgroTeachNova.repository.CountryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    private final CountryRepository countryRepository;
+
+    public CountryService(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
