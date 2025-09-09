f80a3ad05d389b8110d5b7482c8822f7cbec41c3
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:25:02 -0500

    Create ProjectController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/ProjectController.java
@@ -0,0 +1,24 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.Project;
+import com.AgrotechNova.AgroTeachNova.service.ProjectService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class ProjectController {
+
+    private final ProjectService ProjectService;
+
+    public ProjectController(ProjectService ProjectService) {
+        this.ProjectService = ProjectService;
+    }
+
+    // Endpoint de prueba para mañana empezar a expandir
+    @GetMapping
+    public List<Project> getAllProject() {
+        return ProjectService.getAllProject();
+    }
+}
