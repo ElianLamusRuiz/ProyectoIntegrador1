a7eb34e5fb8af90667d411b8fbd378688d234278
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:33:40 -0500

    Create ServiceController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/ServiceController.java
@@ -0,0 +1,23 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.Service;
+import com.AgrotechNova.AgroTeachNova.service.ServiceService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/service")
+public class ServiceController {
+
+    private final ServiceService serviceService;
+
+    public ServiceController(ServiceService serviceService) {
+        this.serviceService = serviceService;
+    }
+
+    @GetMapping
+    public List<Service> getAllService() {
+        return serviceService.getAllService();
+    }
+}
