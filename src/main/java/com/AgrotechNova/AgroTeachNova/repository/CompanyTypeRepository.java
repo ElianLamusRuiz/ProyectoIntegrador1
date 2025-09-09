2ebe0002a09144f8d2a623df3caa87863605e97e
ElianLamusRuiz <elianclamus10@gmail.com>
2025-09-08 22:48:09 -0500

    Correcion de error de nombre

--- src/main/java/com/AgrotechNova/AgroTeachNova/repository/CompanyTypeRepository.java
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/CompanyTypeRepository.java
@@ -3,6 +3,6 @@ package com.AgrotechNova.AgroTeachNova.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.AgrotechNova.AgroTeachNova.Company;
 
-public interface CompanyRepository extends JpaRepository<Company,Long> {
+public interface CompanyTypeRepository extends JpaRepository<Company,Long> {
 	
 }
