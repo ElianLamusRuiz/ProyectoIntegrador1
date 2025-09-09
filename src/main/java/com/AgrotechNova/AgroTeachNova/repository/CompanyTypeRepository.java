3ebcdfa468fc8b83a027c9c6a390d1b80a151675
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 22:14:00 -0500

    Create CompanyTypeRepository.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/CompanyTypeRepository.java
@@ -0,0 +1,8 @@
+package com.AgrotechNova.AgroTeachNova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.AgrotechNova.AgroTeachNova.Company;
+
+public interface CompanyRepository extends JpaRepository<Company,Long> {
+	
+}
