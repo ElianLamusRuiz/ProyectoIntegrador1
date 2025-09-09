c28cbedadba007c59e897e9f261b33be2ae867e3
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:32:10 -0500

    Create ResourcesController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/ResourcesController.java
@@ -0,0 +1,23 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.Resources;
+import com.AgrotechNova.AgroTeachNova.service.ResourcesService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/resources")
+public class ResourcesController {
+
+    private final ResourcesService resourcesService;
+
+    public ResourcesController(ResourcesService resourcesService) {
+        this.resourcesService = resourcesService;
+    }
+
+    @GetMapping
+    public List<Resources> getAllResources() {
+        return resourcesService.getAllResources();
+    }
+}
