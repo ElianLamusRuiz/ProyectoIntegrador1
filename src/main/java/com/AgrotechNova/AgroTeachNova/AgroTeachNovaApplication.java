bd286b2ded7c11f805f411d1fae11ed94498d355
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 21:25:48 -0500

    Update AgroTeachNovaApplication.java

--- src/main/java/com/AgrotechNova/AgroTeachNova/AgroTeachNovaApplication.java
+++ src/main/java/com/AgrotechNova/AgroTeachNova/AgroTeachNovaApplication.java
@@ -1,5 +1,6 @@
 package com.AgrotechNova.AgroTeachNova;
 
+import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
