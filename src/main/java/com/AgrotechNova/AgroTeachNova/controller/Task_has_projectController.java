19df23d087c5e0e3fd9ff521a579e8dbec703585
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:39:34 -0500

    Create Task_has_projectController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/Task_has_projectController.java
@@ -0,0 +1,23 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.Task_has_project;
+import com.AgrotechNova.AgroTeachNova.service.Task_has_projectService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/task_has_project")
+public class Task_has_projectController {
+
+    private final Task_has_projectService task_has_projectService;
+
+    public Task_has_projectController(Task_has_projectService task_has_projectService) {
+        this.task_has_projectService = task_has_projectService;
+    }
+
+    @GetMapping
+    public List<Task_has_project> getAllTask_has_project() {
+        return task_has_projectService.getAllTask_has_project();
+    }
+}
