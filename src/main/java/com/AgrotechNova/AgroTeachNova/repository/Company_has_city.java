8657ffdf908bc9ffdf2394fdadc3319bffb7aec2
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 22:15:32 -0500

    Create Company_has_city.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/Company_has_city.java
@@ -0,0 +1,8 @@
+package com.AgrotechNova.AgroTeachNova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.AgrotechNova.AgroTeachNova.Company_has_city;
+
+public interface Company_has_cityRepository extends JpaRepository<Company_has_city,Long> {
+	
+}
