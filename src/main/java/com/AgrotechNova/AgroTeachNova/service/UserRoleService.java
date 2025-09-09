e55ebed03d53aacea62bfba76a14e2e00025ffe3
Daniela080 <142316432+Daniela080@users.noreply.github.com>
2025-09-09 14:30:24 -0500

    Create UserRoleService.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/service/UserRoleService.java
@@ -0,0 +1,21 @@
+package com.AgrotechNova.AgroTeachNova.service;
+
+import com.AgrotechNova.AgroTeachNova.UserRole;
+import com.AgrotechNova.AgroTeachNova.repository.UserRoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserRoleService {
+
+    private final UserRoleRepository userRoleRepository;
+
+    public UserRoleService(UserRoleRepository userRoleRepository) {
+        this.userRoleRepository = userRoleRepository;
+    }
+
+    public List<UserRole> getAllUserRoles() {
+        return userRoleRepository.findAll();
+    }
+}
