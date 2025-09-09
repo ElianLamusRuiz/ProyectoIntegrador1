a781d075533b5fb97fd37e48383a187aeb5b22b5
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 20:27:37 -0500

    Create resourceType.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/resourceType.java
@@ -0,0 +1,38 @@
+public class ResourceType {
+    private int idresourceType;
+    private String name;
+    private String description;
+
+    public ResourceType() {
+    }
+
+    public ResourceType(int idresourceType, String name, String description) {
+        this.idresourceType = idresourceType;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getIdresourceType() {
+        return idresourceType;
+    }
+
+    public void setIdresourceType(int idresourceType) {
+        this.idresourceType = idresourceType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
