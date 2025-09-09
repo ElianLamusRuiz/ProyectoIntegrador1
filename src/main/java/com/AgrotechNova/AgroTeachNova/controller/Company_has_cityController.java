ccb7681d219a750fae78d6b11378835ce3f3e166
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:15:12 -0500

    Create Company_has_cityController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/Company_has_cityController.java
@@ -0,0 +1,24 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.Company_has_city;
+import com.AgrotechNova.AgroTeachNova.service.Company_has_cityService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class Company_has_cityController {
+
+    private final Company_has_cityService Company_has_cityService;
+
+    public Company_has_cityController(Company_has_cityService Company_has_cityService) {
+        this.Company_has_cityService = Company_has_cityService;
+    }
+
+    // Endpoint de prueba para mañana empezar a expandir
+    @GetMapping
+    public List<Company_has_city> getAllCompany_has_city() {
+        return Company_has_cityService.getAllCompany_has_city();
+    }
+}
