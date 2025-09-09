aa8511f3669e76bbad402728bee68cdb9013a444
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:31:36 -0500

    Create ResourceTypeController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/ResourceTypeController.java
@@ -0,0 +1,23 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.ResourceType;
+import com.AgrotechNova.AgroTeachNova.service.ResourceTypeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/resource_type")
+public class ResourceTypeController {
+
+    private final ResourceTypeService resourceTypeService;
+
+    public ResourceTypeController(ResourceTypeService resourceTypeService) {
+        this.resourceTypeService = resourceTypeService;
+    }
+
+    @GetMapping
+    public List<ResourceType> getAllResourceType() {
+        return resourceTypeService.getAllResourceType();
+    }
+}
