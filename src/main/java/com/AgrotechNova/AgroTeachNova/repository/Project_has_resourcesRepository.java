66e67f3286fdb39b86d5021a2b200f109056eae2
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 22:20:39 -0500

    Create Project_has_resourcesRepository.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/Project_has_resourcesRepository.java
@@ -0,0 +1,8 @@
+package com.AgrotechNova.AgroTeachNova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.AgrotechNova.AgroTeachNova.Project_has_resources;
+
+public interface Project_has_resourcesRepository extends JpaRepository<Project_has_resources,Long> {
+	
+}
