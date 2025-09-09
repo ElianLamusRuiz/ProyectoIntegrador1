9f784fffed4cb3c1f106632525a16ced9dbad0d3
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:30:53 -0500

    Create Project_has_resourcesController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/Project_has_resourcesController.java
@@ -0,0 +1,23 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.Project_has_resources;
+import com.AgrotechNova.AgroTeachNova.service.Project_has_resourcesService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/project_has_resources")
+public class Project_has_resourcesController {
+
+    private final Project_has_resourcesService project_has_resourcesService;
+
+    public Project_has_resourcesController(Project_has_resourcesService project_has_resourcesService) {
+        this.project_has_resourcesService = project_has_resourcesService;
+    }
+
+    @GetMapping
+    public List<Project_has_resources> getAllProject_has_resources() {
+        return project_has_resourcesService.getAllProject_has_resources();
+    }
+}
