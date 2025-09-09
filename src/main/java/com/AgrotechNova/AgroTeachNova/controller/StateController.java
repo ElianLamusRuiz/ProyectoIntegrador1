659917a139709a022d2ea9c820eff81f94266c57
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:35:18 -0500

    Create StateController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/StateController.java
@@ -0,0 +1,23 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.State;
+import com.AgrotechNova.AgroTeachNova.service.StateService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/state")
+public class StateController {
+
+    private final StateService stateService;
+
+    public StateController(StateService stateService) {
+        this.stateService = stateService;
+    }
+
+    @GetMapping
+    public List<State> getAllState() {
+        return stateService.getAllState();
+    }
+}
