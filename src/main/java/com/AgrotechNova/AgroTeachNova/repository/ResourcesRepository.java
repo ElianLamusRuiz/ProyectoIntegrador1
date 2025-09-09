ca99ec78cb47b4a7c09c75ea747a0bbf74dc33ac
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 22:25:09 -0500

    Create ResourcesRepository.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/ResourcesRepository.java
@@ -0,0 +1,8 @@
+package com.AgrotechNova.AgroTeachNova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.AgrotechNova.AgroTeachNova.Resources;
+
+public interface ResourcesRepository extends JpaRepository<Resources,Long> {
+
+}
