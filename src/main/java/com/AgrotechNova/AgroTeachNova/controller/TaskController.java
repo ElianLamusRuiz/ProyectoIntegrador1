4710f8720ed4af13ae65b2f8bf2b6a93f8ff02a3
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-09 14:36:47 -0500

    Create TaskController.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/TaskController.java
@@ -0,0 +1,23 @@
+package com.AgrotechNova.AgroTeachNova.controller;
+
+import com.AgrotechNova.AgroTeachNova.Task;
+import com.AgrotechNova.AgroTeachNova.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/task")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTask() {
+        return taskService.getAllTask();
+    }
+}
