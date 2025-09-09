0be458236ba6f584c1a57be6dddfc8a2c0baa17d
pan-ship-it <142315801+pan-ship-it@users.noreply.github.com>
2025-09-08 22:11:40 -0500

    Create CityRepository.java

---
+++ src/main/java/com/AgrotechNova/AgroTeachNova/repository/CityRepository.java
@@ -0,0 +1,8 @@
+package com.AgrotechNova.AgroTeachNova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.AgrotechNova.AgroTeachNova.City;
+
+public interface CityRepository extends JpaRepository<City,Long> {
+	
+}
