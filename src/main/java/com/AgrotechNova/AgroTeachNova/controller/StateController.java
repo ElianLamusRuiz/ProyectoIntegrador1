51cfb23fd9adf08835269a8bb17d8d7f721d272c
Alexer <93394982+Ralex33@users.noreply.github.com>
2025-09-10 08:55:49 -0500

    Update StateController.java

--- src/main/java/com/AgrotechNova/AgroTeachNova/controller/StateController.java
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/StateController.java
@@ -20,4 +20,23 @@ public class StateController {
     public List<State> getAllState() {
         return stateService.getAll();
     }
+     @GetMapping("/{id}")
+    public State getById(@PathVariable Long id) {
+        return stateService.getById(id);
+    }
+
+    @PostMapping
+    public State create(@RequestBody State body) {
+        return stateService.create(body);
+    }
+
+    @PutMapping("/{id}")
+    public State update(@PathVariable Long id, @RequestBody State body) {
+        return stateService.update(id, body);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        stateService.delete(id);
+    }
 }
