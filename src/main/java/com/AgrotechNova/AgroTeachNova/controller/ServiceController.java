542d86edc5b4e2415997e0a54e1a31e0432ff51f
Alexer <93394982+Ralex33@users.noreply.github.com>
2025-09-10 08:53:21 -0500

    Update ServiceController.java

--- src/main/java/com/AgrotechNova/AgroTeachNova/controller/ServiceController.java
+++ src/main/java/com/AgrotechNova/AgroTeachNova/controller/ServiceController.java
@@ -20,4 +20,23 @@ public class ServiceController {
     public List<Service> getAllService() {
         return serviceService.getAll();
     }
+    @GetMapping("/{id}")
+    public Service getById(@PathVariable Long id) {
+        return serviceService.getById(id);
+    }
+
+    @PostMapping
+    public Service create(@RequestBody Service body) {
+        return serviceService.create(body);
+    }
+
+    @PutMapping("/{id}")
+    public Service update(@PathVariable Long id, @RequestBody Service body) {
+        return serviceService.update(id, body);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        serviceService.delete(id);
+    }
 }
